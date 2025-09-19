e69c6fb154f846997776e9da213114a373483bf0
Christopher-Lord <lordc42@gmail.com>
2025-09-19 17:35:53 +0200

    Update formatting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@
  */
 
 // Import all events and main task function
-import "./events.js"
+import "./events.js";
 import { renderTasks } from "./tasks.js";
 
 // Only runs renderTasks once the DOM is loaded
