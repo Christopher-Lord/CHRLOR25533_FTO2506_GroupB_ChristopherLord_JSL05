b72a4ed3a9a0ba5bda5d7895129a598b96f1cfb3
Christopher-Lord <lordc42@gmail.com>
2025-09-19 17:39:59 +0200

    Update retriveTasksFromStorage
    Update function to grab initial task data if there is noting saved in local storage

--- scripts/storage.js
+++ scripts/storage.js
@@ -3,7 +3,7 @@
  * Handles saving data to and retrieving data from local storage
  */
 
-import { allTasks } from "./initialData.js";
+import { initialTasks, allTasks } from "./initialData.js";
 
 /**
  * Saves array to local storage
@@ -14,10 +14,20 @@ export function saveTasksToStorage() {
 }
 
 /**
- * Retrieves tasks from local storage, empties allTasks array and repopulates it with tasks from storage
+ *  Retrieves tasks from local storage, empties allTasks array and repopulates it with tasks from storage
+ * @returns allTasks array, populated with data from storage
  */
 export function retrieveTasksFromStorage() {
   let savedTasks = localStorage.getItem("allTasks");
+
+  if (!savedTasks) {
+    initialTasks.forEach((task) => {
+      allTasks.push(task);
+    });
+    saveTasksToStorage();
+    return allTasks;
+  }
+
   try {
     let parsedTasks = JSON.parse(savedTasks);
     allTasks.length = 0;
@@ -27,4 +37,6 @@ export function retrieveTasksFromStorage() {
   } catch (error) {
     console.error("Error parsing JSON", error);
   }
+
+  return allTasks;
 }
